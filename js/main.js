fcf69abe5eafda67a51b3d8bc4a01adcd7f141c5
rumi011 <abudarda1166@gmail.com>
2022-08-30 13:01:36 +0600

    Dashboard design

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function profile(evt, profileTab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(profileTab).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
